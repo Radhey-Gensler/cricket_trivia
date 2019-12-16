94ea3ffa1be5c1264cbb4f5b20e46b475885c9a7
Radhey Reddy <radheyreddy.3001@gmail.com>
2019-12-16 08:59:27 +0530

    used json data

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import questionere from "../../Data/data";
 import "./Quiz.css";
 class Quiz extends Component {
+  renderQuestions() {
+    return questionere.quiz.map((quest, index) => {
+      return <div key={index}>{quest.question}</div>;
+    });
+  }
   render() {
-    return <div>Quiz goes here</div>;
+    return <React.Fragment>{this.renderQuestions()}</React.Fragment>;
   }
 }
 
