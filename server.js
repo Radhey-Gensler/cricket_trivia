9d5533c3b9e173a5fd22373461342e4c1d6adaf4
Radhey <radheyreddy.3001@gmail.com>
2019-12-17 07:22:11 +0530

    trying to deploy to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
+const https = require('https');
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 8080;
@@ -15,6 +16,10 @@ console.log(__dirname, path.join(__dirname,'dist'))
 app.use(express.static(path.join(__dirname,'dist')));
 
 
-http.createServer(app,(req,res)=>{
+/* http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+}) */
+
+https.createServer(app,(req,res)=>{
 
 }).listen(port,()=> console.log("listening on port",port));
